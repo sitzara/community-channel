8b4cfff7259774d8df4e9cc8334b85ed3f5f5052
sitzara <sitzara@gmail.com>
2023-04-27 00:42:00 +0200

    feat: add users resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { RoomUsersModule } from './room-users/room-users.module';
 import { RoomMessagesModule } from './room-messages/room-messages.module';
@@ -9,6 +10,7 @@ import { RoomMessagesModule } from './room-messages/room-messages.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/local'),
+    UsersModule,
     RoomsModule,
     RoomUsersModule,
     RoomMessagesModule,
---
+++ src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  create(createUserDto: CreateUserDto) {
+    const { name } = createUserDto;
+    const user = this.userModel.create({ name });
+
+    console.log('User created', user);
+    return user;
+  }
+}
